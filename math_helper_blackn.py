71877158e6012e73dc35204aaefd9f595d80a506
Nathan Black <2210262@flourbluffschools.org>
2018-09-12 14:11:41 -0500

    change made

--- math_helper_blackn.py
+++ math_helper_blackn.py
@@ -1,5 +1,28 @@
 
+def determineReferenceAngle(numer, denom): #used within unitCircle()
+    degAngle = (numer * 180) // denom
+    if degAngle <= 90:
+        referenceAngle = degAngle
+    elif 
 
-def unitCircle(function, x,y):
-    '''returns the value (in simplest radical form) of
+
+
+
+
+
+
+def unitCircle(trigOp, numer, denom):
+    '''returns the value (in simplest radical form and decimal if
+       numer and denom correspond with points on the unit circle, just
+       decimal if not) of
+       
+             (numer * pi)
+       trigOp(----------)
+             (  denom   )                                          '''
+    
+    
+    
+    
+         
+        
        

28452c87522e7097ca2cc13409413a20bf7f7ebc
Nathan Black <2210262@flourbluffschools.org>
2018-09-13 13:53:46 -0500

    unitcircle() add errors

--- math_helper_blackn.py
+++ math_helper_blackn.py
@@ -1,9 +1,5 @@
 
-def determineReferenceAngle(numer, denom): #used within unitCircle()
-    degAngle = (numer * 180) // denom
-    if degAngle <= 90:
-        referenceAngle = degAngle
-    elif 
+ 
 
 
 
@@ -19,6 +15,27 @@ def unitCircle(trigOp, numer, denom):
              (numer * pi)
        trigOp(----------)
              (  denom   )                                          '''
+    
+    referenceAngles = [0, 30, 45, 60, 90]
+    trigOperations = ["sine", "sin", "cosine", "cos", "tangent", "tan", "cosecant", "csc", "secant", "sec", "cotangent", "tan"]
+    degAngle = ((numer//1) * 180) // (denom//1)
+    if degAngle <= 90:
+        referenceAngle = degAngle
+    elif degAngle <= 180:
+        referenceAngle = 180 - degAngle
+    elif degAngle <= 270:
+        referenceAngle = degAngle - 180
+    elif degAngle <= 360:
+        referenceAngle = 360 - degAngle
+    if degAngle > 360 or degAngle < 0 or referenceAngle not in referenceAngles:
+        raise ValueError("point is not on the unit circle.")
+    if trigOp not in trigOperations:
+        raise ValueError("not a valid trigonometric operation.")
+    
+    
+    
+    
+    
     
     
     
